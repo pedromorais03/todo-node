1840499cb559fa5f6f85cda92f62b0086a1161fc
Pedro Henrique Morais Pereira <pedromorais3012@gmail.com>
2024-10-12 20:45:07 -0300

    configuracao de rota com middleware do redis

--- back/routes/tasks.js
+++ back/routes/tasks.js
@@ -17,20 +17,20 @@ router.post('/', async (req, res) => {
 })
 
 router.get('/', async (req, res) => {
-   const tasks = await Task.find();
-   res.status(200).json(tasks);
+   const tasks = await Task.find()
+   res.status(200).json(tasks)
 })
 
 router.put('/:id', async (req, res) => {
-   const { id } = req.params;
-   const { completed } = req.body;
-   const task = await Task.findByIdAndUpdate(id, { completed }, { new: true });
+   const { id } = req.params
+   const { completed } = req.body
+   const task = await Task.findByIdAndUpdate(id, { completed }, { new: true })
    res.status(200).json(task);
 })
 
 router.delete('/:id', async (req, res) => {
-   const { id } = req.params;
-   await Task.findByIdAndDelete(id);
-   res.status(200).json({ message: 'Tarefa removida' });
+   const { id } = req.params
+   await Task.findByIdAndDelete(id)
+   res.status(200).json({ message: 'Tarefa removida' })
 })
  
--- back/server.js
+++ back/server.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const redis  = require('redis')
 
 const taskRoutes = require('./routes/tasks')
+const Task = require('./models/Task')
 const app = express()
 const port = process.env.PORT || 3000
 
@@ -11,10 +12,22 @@ const client = redis.createClient()
 
 client.on('error', (err) => console.log('Erro de cliente Redis', err))
 
+(async () => {
+   await client.connect()
+})()
+
 // middlewares
 app.use(express.json())
 app.use(cors())
 app.use('/api/tasks', taskRoutes)
+
+const cache = async (req, res, next) => {
+   const tasks = await client.get('tasks')
+   if (tasks) {
+      return res.status(200).json(JSON.parse(tasks))
+   }
+   next()
+}
 
 // mongoose.connect(process.env.URI_MONGO, {
 //    useNewUrlParser: true,
@@ -24,6 +37,13 @@ app.use('/api/tasks', taskRoutes)
 // rotas da aplicação
 app.get('/', (req, res) => {
    res.send('API funcionando')
+})
+
+app.get('/api/tasks', cache, async(req, res) => {
+   const tasks = await Task.find()
+   await client.set('tasks', JSON.stringify(tasks))
+
+   res.status(200).json(tasks)
 })
 
 app.listen(port, () => console.log(`Servidor na porta ${port}`))
